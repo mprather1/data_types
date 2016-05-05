9749d91d3fddddc09716591cc86c9f35c914734b
Michael Prather <mprather1@gmail.com>
2016-05-05 00:11:48 -0500

    done

---
+++ data_types.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+int main() {
+  char letter = 'a';
+  short age = 10;
+  int count = 575;
+  long numStars = 985467528;
+  float pi = 3.14;
+  double price = 89.65;
+  string season = "summer";
+
+  cout<<"Letter: "<<letter<<endl;
+  cout<<"Age: "<<age<<endl;
+  cout<<"Count: "<<count<<endl;
+  cout<<"Number of stars: "<<numStars<<endl;
+  cout<<"Pi: "<<pi<<endl;
+  cout<<"Price: $"<<price<<endl;
+  cout<<"Season: "<<season<<endl;
+
+  return 0;
+}
